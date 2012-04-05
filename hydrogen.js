f0f0a9d6905a00710cd708fc5b1918fe27d4d530
Kirill Klenov <horneds@gmail.com>
2012-04-05 18:35:37 +0400

    Add atom.Template.

--- hydrogen.js
+++ hydrogen.js
@@ -1,3 +1,4 @@
 require('src/router.js');
+require('src/template.js');
 require('src/view.js');
 require('src/model.js');
---
+++ src/template.js
@@ -0,0 +1,61 @@
+// JavaScript micro-templating, similar to John Resig's implementation.
+
+(function(atom){
+    
+    var evaluate = /<%([\s\S]+?)%>/g,
+        interpolate = /<%=([\s\S]+?)%>/g,
+        escape = /<%-([\s\S]+?)%>/g,
+        noMatch = /.^/,
+        unescape = function(code) { return code.replace(/\\\\/g, '\\').replace(/\\'/g, "'"); };
+
+    atom.declare('atom.Template', {
+
+        tmpl: null,
+
+        initialize: function(el, defaults){
+            this.el = atom.dom(el);
+            this.defaults = defaults || {};
+        },
+
+        compile: function(){
+            var str = this.el.html();
+            this.tmpl = this.constructor.compile(str);
+            return this.tmpl;
+        },
+
+        render: function(data){
+            var tmpl = this.tmpl || this.compile(),
+                context = atom.core.append(this.defaults, data || {});
+
+            return tmpl(data);
+        },
+
+        own: {
+
+            compile: function(str, data) {
+                var tmpl = 'var __p=[],print=function(){__p.push.apply(__p,arguments);};' +
+                    'with(obj||{}){__p.push(\'' +
+                    str
+                        .replace(/\\/g, '\\\\')
+                        .replace(/'/g, "\\'")
+                        .replace(escape || noMatch, function(match, code) {
+                            return "',_.escape(" + unescape(code) + "),'";
+                        })
+                        .replace(interpolate || noMatch, function(match, code) {
+                            return "'," + unescape(code) + ",'";
+                        })
+                        .replace(evaluate || noMatch, function(match, code) {
+                            return "');" + unescape(code).replace(/[\r\n\t]/g, ' ') + ";__p.push('";
+                        })
+                        .replace(/\r/g, '\\r')
+                        .replace(/\n/g, '\\n')
+                        .replace(/\t/g, '\\t')
+                    + "');}return __p.join('');";
+                var func = new Function('obj', '_', tmpl);
+                if (data) return func(data, _);
+                return function(data) { return func.call(this, data, _); };
+            }
+        }
+    });
+
+})(atom);
