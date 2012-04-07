f6129760d827afb74132e5e83a2aa84b8868e10a
Kirill Klenov <horneds@gmail.com>
2012-04-07 16:41:57 +0400

    Jslint

--- src/model.js
+++ src/model.js
@@ -1,133 +1,138 @@
-atom.declare('hydrogen.Model', {
+(function () {
+    "use strict";
 
-    parent: hydrogen.Base,
+    atom.declare('hydrogen.Model', {
 
-    own: {
-        extend: function(proto) {
-            return atom.declare({
-                parent: hydrogen.Model,
-                proto: proto
-            });
-        }
-    },
+        parent: hydrogen.Base,
 
-    proto: {
-
-        properties: 'collection'.split(' '),
-
-        idAttribute: 'id',
-
-        initialize: function parent (attrs, settings){
-            parent.previous.call(this, settings);
-
-            this.attrs = new atom.Settings()
-                            .set(this.defaults)
-                            .set(attrs);
-            this.configure();
-        },
-
-        get id(){ return this.get(this.idAttribute); },
-
-        set id(value){ this.set(this.idAttribute, value); },
-
-        get: function(name){
-            return this.attrs.get(name);
-        },
-
-        set: function(name, value){
-            this._id = this.id;
-            this.attrs.set(name, value);
-            this.fire('change', [this, this.collection, arguments]);
-        },
-
-        unset: function(name){
-            this.set(name, null);
-        },
-
-        has: function(name){
-            return this.get(name) !== undefined;
-        },
-
-        sync: function(){
-            return hydrogen.sync.apply(this, arguments);
-        },
-
-        toJSON: function() {
-            return atom.clone(this.attrs.values);
-        },
-
-        parse: function(resp, xhr) {
-            return resp;
-        },
-
-        isNew: function() {
-            return this.id === undefined || this.id === null;
-        },
-
-        url: function() {
-            var base = this.urlRoot || this.collection.url;
-            if (this.isNew()) { return base; }
-            return base + (base.charAt(base.length - 1) == '/' ? '' : '/') + encodeURIComponent(this.id);
-        },
-
-        clone: function(){
-            return new this.constructor(this.attrs.values, this.settings.values);
-        },
-
-        save: function(key, value, settings) {
-            var attrs, current, model=this;
-
-            if (key === Object(key) || key === null) {
-                attrs = key;
-                settings = value;
-            } else {
-                attrs = {};
-                attrs[key] = value;
+        own: {
+            extend: function (proto) {
+                return atom.declare({
+                    parent: hydrogen.Model,
+                    proto: proto
+                });
             }
-            settings = atom.append({}, settings);
-
-            var onLoad = settings.onLoad;
-            settings.onLoad = function(resp) {
-                var serverAttrs = model.parse(resp);
-                model.set(model.parse(resp));
-                if (onLoad) {
-                    onLoad(model, resp);
-                } else {
-                    model.fire('sync', [resp, settings]);
-                }
-            };
-
-            var method = this.isNew() ? 'create' : 'update';
-            return this.sync(method, this, settings);
         },
 
-        destroy: function(settings) {
-            var model = this;
+        proto: {
 
-            settings = atom.append({}, settings);
+            properties: ['collection'],
 
-            var onLoad = settings.onLoad;
+            idAttribute: 'id',
 
-            var triggerDestroy = function() {
-                model.fire('destroy', [model, model.collection, settings]);
-            };
+            initialize: function parent(attrs, settings) {
+                parent.previous.call(this, settings);
 
-            if (this.isNew()) {
+                this.attrs = new atom.Settings()
+                                .set(this.defaults)
+                                .set(attrs);
+                this.configure();
+            },
+
+            get id() { return this.get(this.idAttribute); },
+
+            set id(value) { this.set(this.idAttribute, value); },
+
+            get: function (name) {
+                return this.attrs.get(name);
+            },
+
+            set: function (name, value, settings) {
+                console.log('model.set.start');
+                var options = settings || {};
+                this._id = this.id;
+                console.log(name, value, settings);
+                this.attrs.set(name, value);
+                if (!options.silent) { this.fire('change', [this, this.collection, arguments]); }
+                console.log('model.set.end');
+            },
+
+            unset: function (name, settings) {
+                console.log('model.unset.start');
+                this.set(name, null, settings);
+                console.log('model.unset.end');
+            },
+
+            has: function (name) {
+                return this.get(name) !== undefined;
+            },
+
+            toJSON: function () {
+                return atom.clone(this.attrs.values);
+            },
+
+            parse: function (resp, xhr) {
+                return resp;
+            },
+
+            isNew: function () {
+                return this.id === undefined || this.id === null;
+            },
+
+            url: function () {
+                var base = this.urlRoot || this.collection.url;
+                if (this.isNew()) { return base; }
+                return base + (base.charAt(base.length - 1) == '/' ? '' : '/') + encodeURIComponent(this.id);
+            },
+
+            clone: function () {
+                return new this.constructor(this.attrs.values, this.settings.values);
+            },
+
+            save: function (key, value, settings) {
+                console.log('model.save');
+                var attrs, current, model = this, onLoad, method;
+
+                if (key === {}(key) || key === null) {
+                    attrs = key;
+                    settings = value;
+                } else {
+                    attrs = {};
+                    attrs[key] = value;
+                }
+                settings = atom.clone(settings || {});
+                onLoad = settings.onLoad;
+                settings.onLoad = function (resp) {
+                    var serverAttrs = model.parse(resp);
+                    model.set(model.parse(resp));
+                    if (onLoad) {
+                        onLoad(model, resp);
+                    } else {
+                        model.fire('sync', [resp, settings]);
+                    }
+                };
+                method = this.isNew() ? 'create' : 'update';
+                return this.sync(method, this, settings);
+            },
+
+            destroy: function (settings) {
+                var model = this, onLoad, triggerDestroy, xhr;
+
+                settings = atom.clone(settings || {});
+
+                onLoad = settings.onLoad;
+
+                triggerDestroy = function () {
+                    model.fire('destroy', [model, model.collection, settings]);
+                };
+
+                if (this.isNew()) {
+                    triggerDestroy();
+                    return false;
+                }
+
+                settings.onLoad = function (resp) {
+                    if (onLoad) {
+                        onLoad(model, resp);
+                    } else {
+                        model.fire('sync', [resp, settings]);
+                    }
+                };
+
+                xhr = this.sync('delete', this, settings);
                 triggerDestroy();
-                return false;
+                return xhr;
             }
-
-            settings.onLoad = function(resp) {
-                if (onLoad) {
-                    onLoad(model, resp);
-                } else {
-                    model.fire('sync', [resp, settings]);
-                }
-            };
-
-            var xhr = this.sync('delete', this, settings);
-            triggerDestroy();
-            return xhr;
         }
-    }
-});
+    });
+}());
