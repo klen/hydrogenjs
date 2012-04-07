8aaaecc02353131da16b595cd6de5bfc6ef2be00
Kirill Klenov <horneds@gmail.com>
2012-04-07 16:28:51 +0400

    Jslint

--- src/collection.js
+++ src/collection.js
@@ -1,204 +1,221 @@
-atom.declare('hydrogen.Collection', {
+(function () {
+    "use strict";
 
-    parent: hydrogen.Base,
+    atom.declare('hydrogen.Collection', {
 
-    own: {
-        extend: function(proto) {
-            return atom.declare({
-                parent: hydrogen.Collection,
-                proto: proto
-            });
-        }
-    },
+        parent: hydrogen.Base,
 
-    proto: {
-
-        properties: 'id model comparator url'.split(' '),
-
-        model: hydrogen.Model,
-
-        initialize: function parent (settings){
-
-            this.bindMethods('_onModelEvent', '_removeReference');
-
-            parent.previous.apply(this, arguments);
-
-            this.models = [];
-            this.reset(this.settings.get('models') || [], settings);
-            this.configure();
+        own: {
+            extend: function (proto) {
+                return atom.declare({
+                    parent: hydrogen.Collection,
+                    proto: proto
+                });
+            }
         },
 
-        toJSON: function() {
-            return this.models.map(function(model){ return model.toJSON(); });
-        },
+        proto: {
 
-        get length () {
-            return this.models.length;
-        },
+            properties: ['id', 'model', 'comparator', 'url'],
 
-        set length (value) {},
+            model: hydrogen.Model,
 
-        get first() { return this.models[0]; },
+            initialize: function parent(settings) {
 
-        get last() { return this.models[this.length-1] },
+                this.bindMethods('_onModelEvent', '_removeReference');
 
-        get: function(id){
-            if (id == null) return void 0;
-            return this._byId[id.id != null ? id.id : id];
-        },
+                parent.previous.apply(this, arguments);
 
-        getByCid: function(cid){
-            return cid && this._byCid[cid.cid || cid];
-        },
+                this.models = [];
+                this.reset(this.settings.get('models') || [], settings);
+                this.configure();
+            },
 
-        at: function(index) {
-            return this.models[index];
-        },
+            toJSON: function () {
+                return this.models.map(function (model) { return model.toJSON(); });
+            },
 
-        pluck: function(name){
-            return this.models.map(function(model){ return model.get(name) });
-        },
+            get length () {
+                return this.models.length;
+            },
 
-        sort: function(settings) {
-            settings || (settings = {});
-            if (!this.comparator) throw new Error('Cannot sort a set without a comparator');
-            var boundComparator = this.comparator.bind(this);
-            this.models.sort(boundComparator);
-            if (!settings.silent) this.fire('reset', [this, settings]);
-            return this;
-        },
+            set length (value) {
+                this.models.length = value;
+            },
 
-        add: function(models, settings){
-            settings = atom.append({}, settings);
+            get first() { return this.models[0]; },
 
-            var mds = [], index,
-                _byId = this._byId, _byCid = this._byCid, col=this,
-                _onModelEvent = this._onModelEvent,
-                _prepareModel = this._prepareModel.bind(this);
+            set first(value) { throw "Not allowed"; },
 
-            models = atom.isArrayLike(models) ? models.slice() : [models];
-            models.forEach(function(model){
-                model = _prepareModel(model);
-                model.events.add('all', _onModelEvent);
-                if (!_byId[model.id] && !_byCid[model.cid]){ 
-                    _byCid[model.cid] = model;
-                    if (model.id != null) { _byId[model.id] = model; }
-                    mds.push(model);
+            get last() { return this.models[this.length - 1]; },
+
+            set last(value) { throw "Not allowed"; },
+
+            get: function (id) {
+                if (id == null) { return undefined; }
+                return this._byId[id.id != null ? id.id : id];
+            },
+
+            getByCid: function (cid) {
+                return cid && this._byCid[cid.cid || cid];
+            },
+
+            at: function (index) {
+                return this.models[index];
+            },
+
+            pluck: function (name) {
+                return this.models.map(function (model) { return model.get(name); });
+            },
+
+            sort: function (settings) {
+                console.log(this + 'sort');
+                settings = settings || {};
+                if (!this.comparator) { throw new Error('Cannot sort a set without a comparator'); }
+                var boundComparator = this.comparator.bind(this);
+                this.models.sort(boundComparator);
+                if (!settings.silent) { this.fire('reset', [this, settings]); }
+                return this;
+            },
+
+            add: function (models, settings) {
+                console.log('collection.add');
+                settings = atom.clone(settings || {});
+
+                var mds = [], index,
+                    _byId = this._byId, _byCid = this._byCid, col = this,
+                    _onModelEvent = this._onModelEvent,
+                    _prepareModel = this._prepareModel.bind(this);
+
+                models = atom.isArrayLike(models) ? models.slice() : [models];
+                models.forEach(function (model) {
+                    model = _prepareModel(model);
+                    model.events.add('all', _onModelEvent);
+                    if (!_byId[model.id] && !_byCid[model.cid]) {
+                        _byCid[model.cid] = model;
+                        if (model.id != null) { _byId[model.id] = model; }
+                        mds.push(model);
+                    }
+                });
+                index = settings.at != null ? settings.at : this.length;
+                Array.prototype.splice.apply(this.models, [index, 0].concat(mds));
+                mds.forEach(function (model) {
+                    settings.index = index++;
+                    if (!settings.silent) { model.fire('add', [model, col, settings]); }
+                });
+                return this;
+            },
+
+            create: function (model, settings) {
+                console.log('collection.create');
+                settings = atom.clone(settings || {});
+
+                var coll = this, onLoad = settings.onLoad;
+                model = this._prepareModel(model, settings);
+                if (!model) { return false; }
+                this.add(model, settings);
+                settings.onLoad = function (nextModel, resp, xhr) {
+                    if (onLoad) {
+                        onLoad(nextModel, resp);
+                    } else {
+                        nextModel.fire('sync', [resp, settings]);
+                    }
+                };
+                model.save(null, settings);
+                return model;
+            },
+
+            remove: function (models, settings) {
+                console.log('collection.remove');
+                var i, l, index, model;
+                settings = settings || {};
+                models = atom.isArrayLike(models) ? models.slice() : [models];
+                for (i = 0, l = models.length; i < l; i++) {
+                    model = this.getByCid(models[i]) || this.get(models[i]);
+                    if (!model) { continue; }
+                    delete this._byId[model.id];
+                    delete this._byCid[model.cid];
+                    index = this.models.indexOf(model);
+                    this.models.splice(index, 1);
+                    this.length--;
+                    this._removeReference(model);
                 }
-            });
-            index = settings.at != null ? settings.at : this.length;
-            Array.prototype.splice.apply(this.models, [index, 0].concat(mds));
-            mds.forEach(function(model){
-                settings.index = index++;
-                settings.silent || model.fire('add', [model, col, settings]); });
-            return this;
-        },
+                return this;
+            },
 
-        create: function(model, settings) {
-            settings = atom.append({}, settings);
+            fetch: function (settings) {
+                console.log('collection.fetch');
+                settings = atom.clone(settings || {});
+                if (settings.parse === undefined) { settings.parse = true; }
 
-            var coll = this;
-            model = this._prepareModel(model, settings);
-            if (!model) return false;
-            this.add(model, settings);
-            var onLoad = settings.onLoad;
-            settings.onLoad = function(nextModel, resp, xhr) {
-                if (onLoad) {
-                    onLoad(nextModel, resp);
-                } else {
-                    nextModel.fire('sync', [resp, settings]);
+                var collection = this,
+                    onLoad = settings.onLoad;
+
+                settings.onLoad = function (resp) {
+                    collection[settings.add ? 'add' : 'reset'](collection.parse(resp), settings);
+                    if (onLoad) { onLoad(collection, resp); }
+                };
+                return this.sync('read', this, settings);
+            },
+
+            parse: function (resp) {
+                console.log('collection.parse');
+                return resp;
+            },
+
+            reset: function (models, settings) {
+                console.log('collection.reset.start');
+                settings = settings || {};
+                this.models.forEach(this._removeReference);
+                this._reset();
+                this.add(models, atom.extend({silent: true}, settings));
+                if (!settings.silent) { this.fire('reset', [models, settings]); }
+                console.log('collection.reset.end');
+            },
+
+            /** @private */
+            _reset: function () {
+                this.length = 0;
+                this.models = [];
+                this._byId  = {};
+                this._byCid = {};
+            },
+
+            /** @private */
+            _prepareModel: function (model, settings) {
+                console.log('collection.prepare');
+                settings = atom.clone(settings || {});
+                if (!(model instanceof hydrogen.Model)) {
+                    var args = model;
+                    settings.collection = this;
+                    model = new this.model(args, settings);
+                } else if (!model.collection) {
+                    model.collection = this;
                 }
-            };
-            model.save(null, settings);
-            return model;
-        },
+                return model;
+            },
 
-        remove: function(models, settings) {
-            var i, l, index, model;
-            settings || (settings = {});
-            models = atom.isArrayLike(models) ? models.slice() : [models];
-            for (i = 0, l = models.length; i < l; i++) {
-                model = this.getByCid(models[i]) || this.get(models[i]);
-                if (!model) continue;
-                delete this._byId[model.id];
-                delete this._byCid[model.cid];
-                index = this.models.indexOf(model);
-                this.models.splice(index, 1);
-                this.length--;
-                this._removeReference(model);
+            /** @private */
+            _onModelEvent: function (event, model, collection, settings) {
+                console.log('collection.onevent.start', event);
+                if ((event == 'add' || event == 'remove') && collection != this) { return; }
+                if (event == 'destroy') {
+                    this.remove(model, settings);
+                }
+                if (event == 'change') {
+                    delete this._byId[model._id];
+                    this._byId[model.id] = model;
+                }
+                this.fire(event, [model, collection, settings]);
+                console.log('collection.onevent.end');
+            },
+
+            _removeReference: function (model) {
+                if (this == model.collection) {
+                    delete model.collection;
+                }
+                model.events.remove('all', this._onModelEvent);
             }
-            return this;
-        },
-
-        fetch: function(settings) {
-            settings = atom.append({}, settings);
-            if (settings.parse === undefined) { settings.parse = true; }
-
-            var collection = this;
-            var onLoad = settings.onLoad;
-
-            settings.onLoad = function(resp) {
-                collection[settings.add ? 'add' : 'reset'](collection.parse(resp), settings);
-                if (onLoad) {onLoad(collection, resp);}
-            };
-            return this.sync('read', this, settings);
-        },
-
-        parse: function(resp) {
-            return resp;
-        },
-
-        sync: function(){
-            return hydrogen.sync.apply(this, arguments);
-        },
-
-        reset: function(models, settings){
-            settings = settings || {}
-            this.models.forEach(this._removeReference);
-            this._reset();
-            this.add(models, atom.extend({silent: true}, settings));
-            settings.silent || this.fire('reset', arguments);
-        },
-
-        /** @private */
-        _reset: function() {
-            this.length = 0;
-            this.models = [];
-            this._byId  = {};
-            this._byCid = {};
-        },
-
-        /** @private */
-        _prepareModel: function(model, settings) {
-            settings = atom.append({}, settings);
-            if (!(model instanceof hydrogen.Model)) {
-                var attrs = model;
-                settings.collection = this;
-                model = new this.model(attrs, settings);
-            } else if (!model.collection) {
-                model.collection = this;
-            }
-            return model;
-        },
-
-        /** @private */
-        _onModelEvent: function(event, model, collection, settings){
-            if ((event == 'add' || event == 'remove') && collection != this) return;
-            if (event == 'destroy') {
-                this.remove(model, settings);
-            }
-            if (event == 'change') {
-                delete this._byId[model._id];
-                this._byId[model.id] = model;
-            }
-            this.fire(event, [model, collection, settings]);
-        },
-
-        _removeReference: function(model) {
-            if (this == model.collection) {
-                delete model.collection;
-            }
-            model.events.remove('all', this._onModelEvent);
         }
-    }
-});
+    });
+}());
