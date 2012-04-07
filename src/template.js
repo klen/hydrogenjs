e6d0025be24121ee167539d0bc422dc1802cfecb
Kirill Klenov <horneds@gmail.com>
2012-04-07 16:11:05 +0400

    Jslint

--- src/template.js
+++ src/template.js
@@ -1,26 +1,26 @@
 // JavaScript micro-templating, similar to John Resig's implementation.
 
-(function(atom){
-    
+(function (atom) {
+    "use strict";
     var evaluate = /<%([\s\S]+?)%>/g,
         interpolate = /<%=([\s\S]+?)%>/g,
         escape = /<%-([\s\S]+?)%>/g,
         noMatch = /.^/,
-        unescape = function(code) { return code.replace(/\\\\/g, '\\').replace(/\\'/g, "'"); };
+        unescape = function (code) { return code.replace(/\\\\/g, '\\').replace(/\\'/g, "'"); };
 
     atom.declare('hydrogen.Template', {
 
         tmpl: null,
 
-        initialize: function(el, defaults){
-            var el = typeof el === 'string' ? atom.dom(el) : el,
-                str = el.html();
+        initialize: function (el, defaults) {
+            var elem = typeof el === 'string' ? atom.dom(el) : el,
+                str = elem.html();
 
             this.defaults = defaults || {};
             this.tmpl = this.constructor.compile(str);
         },
 
-        render: function(data){
+        render: function (data) {
             var tmpl = this.tmpl || this.compile(),
                 context = atom.core.append(this.defaults, data || {});
 
@@ -29,28 +29,28 @@
 
         own: {
 
-            compile: function(str, data) {
+            compile: function (str, data) {
                 var tmpl = 'var __p=[],print=function(){__p.push.apply(__p,arguments);};' +
                     'with(obj||{}){__p.push(\'' +
                     str.replace(/\\/g, '\\\\')
-                       .replace(/'/g, "\\'")
-                       .replace(escape || noMatch, function(match, code) {
-                            return "',hydrogen.escape(" + unescape(code) + "),'";
-                       })
-                       .replace(interpolate || noMatch, function(match, code) {
-                            return "'," + unescape(code) + ",'";
-                       })
-                       .replace(evaluate || noMatch, function(match, code) {
-                            return "');" + unescape(code).replace(/[\r\n\t]/g, ' ') + ";__p.push('";
-                       })
-                       .replace(/\r/g, '\\r')
-                       .replace(/\n/g, '\\n')
-                       .replace(/\t/g, '\\t') + "');}return __p.join('');";
-                var func = new Function('obj', 'hydrogen', tmpl);
+                    .replace(/'/g, "\\'")
+                    .replace(escape || noMatch, function (match, code) {
+                        return "',hydrogen.escape(" + unescape(code) + "),'";
+                    })
+                    .replace(interpolate || noMatch, function (match, code) {
+                        return "'," + unescape(code) + ",'";
+                    })
+                    .replace(evaluate || noMatch, function (match, code) {
+                        return "');" + unescape(code).replace(/[\r\n\t]/g, ' ') + ";__p.push('";
+                    })
+                    .replace(/\r/g, '\\r')
+                    .replace(/\n/g, '\\n')
+                    .replace(/\t/g, '\\t') + "');}return __p.join('');",
+                    func = new Function('obj', 'hydrogen', tmpl);
                 if (data) { return func(data, hydrogen); }
-                return function(data) { return func.call(this, data, hydrogen); };
+                return function (data) { return func.call(this, data, hydrogen); };
             }
         }
     });
 
-})(atom);
+}(atom));
