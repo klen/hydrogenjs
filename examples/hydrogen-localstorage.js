240175094cfafc9f2559ecb54c1220a109183856
Kirill Klenov <horneds@gmail.com>
2012-04-07 19:53:47 +0400

    Fix examples.

--- examples/hydrogen-localstorage.js
+++ examples/hydrogen-localstorage.js
@@ -1,74 +1,90 @@
-atom.declare('hydrogen.Store', {
+/*global atom, hydrogen, console */
 
-    initialize: function(name){
-        var store = localStorage.getItem(name);
+(function () {
+    "use strict";
 
-        this.name = name;
-        this.data = (store && JSON.parse(store)) || {};
-    },
+    atom.declare('hydrogen.Store', {
 
-    save: function() {
-        localStorage.setItem(this.name, JSON.stringify(this.data));
-    },
+        initialize: function (name) {
+            var store = localStorage.getItem(name);
 
-    create: function(model) {
-        if (!model.id) model.set('id', this.constructor.guid(), {
-            silent: true
-        });
-        this.data[model.id] = model;
-        this.save();
-        return model.toJSON();
-    },
-
-    update: function(model) {
-        this.data[model.id] = model;
-        this.save();
-        return model.toJSON();
-    },
-
-    find: function(model) {
-        return this.data[model.id];
-    },
-
-    findAll: function() {
-        return Object.values(this.data);
-    },
-
-    destroy: function(model) {
-        delete this.data[model.id];
-        this.save();
-        return model;
-    },
-
-    own: {
-
-        S4: function(){
-            return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+            this.name = name;
+            this.data = (store && JSON.parse(store)) || {};
         },
 
-        guid: function(){
-            var S4 = hydrogen.Store.S4;
-            return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+        save: function () {
+            localStorage.setItem(this.name, JSON.stringify(this.data));
+        },
+
+        create: function (model) {
+            if (!model.id) { model.set('id', this.constructor.guid(), {
+                silent: true
+            }); }
+            this.data[model.id] = model;
+            this.save();
+            return model.toJSON();
+        },
+
+        update: function (model) {
+            this.data[model.id] = model;
+            this.save();
+            return model.toJSON();
+        },
+
+        find: function (model) {
+            return this.data[model.id];
+        },
+
+        findAll: function () {
+            return Object.values(this.data);
+        },
+
+        destroy: function (model) {
+            delete this.data[model.id];
+            this.save();
+            return model;
+        },
+
+        own: {
+
+            S4: function () {
+                return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+            },
+
+            guid: function () {
+                var S4 = hydrogen.Store.S4;
+                return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+            }
+
         }
 
-    }
+    });
 
-});
+    hydrogen.sync = function (method, model, options) {
+
+        var resp,
+            store = model.localStorage || model.collection.localStorage;
+
+        switch (method) {
+        case "read":
+            resp = model.id ? store.find(model) : store.findAll();
+            break;
+        case "create":
+            resp = store.create(model);
+            break;
+        case "update":
+            resp = store.update(model);
+            break;
+        case "delete":
+            resp = store.destroy(model);
+            break;
+        }
+
+        if (resp) {
+            options.onLoad(resp);
+        }
+    };
+}());
 
 
-hydrogen.sync = function(method, model, options) {
 
-    var resp;
-    var store = model.localStorage || model.collection.localStorage;
-
-    switch (method) {
-        case "read":    resp = model.id ? store.find(model) : store.findAll(); break;
-        case "create":  resp = store.create(model);                            break;
-        case "update":  resp = store.update(model);                            break;
-        case "delete":  resp = store.destroy(model);                           break;
-    }
-
-    if (resp) {
-        options.onLoad(resp);
-    } 
-};
