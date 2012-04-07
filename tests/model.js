6f16623b53bafbc3b8a009c78a21257b4e0c25fc
Kirill Klenov <horneds@gmail.com>
2012-04-07 04:26:52 +0400

    Fix tests.

--- tests/model.js
+++ tests/model.js
@@ -46,7 +46,7 @@ atom.dom(function(){
                 equal(this.collection.cid, collection.cid);
             }
         });
-        var model = new Model({collection: collection});
+        var model = new Model({}, {collection: collection});
         equal(model.one, 1);
         equal(model.collection.cid, collection.cid);
     });
@@ -59,5 +59,47 @@ atom.dom(function(){
         doc.collection = null;
         raises(function() { doc.url(); });
         doc.collection = collection;
+    });
+
+    test("Model: clone", function() {
+        var a = new hydrogen.Model({ 'foo': 1, 'bar': 2, 'baz': 3});
+        var b = a.clone();
+        equal(a.get('foo'), 1);
+        equal(a.get('bar'), 2);
+        equal(a.get('baz'), 3);
+        equal(b.get('foo'), a.get('foo'), "Foo should be the same on the clone.");
+        equal(b.get('bar'), a.get('bar'), "Bar should be the same on the clone.");
+        equal(b.get('baz'), a.get('baz'), "Baz should be the same on the clone.");
+        a.set({foo : 100});
+        equal(a.get('foo'), 100);
+        equal(b.get('foo'), 1, "Changing a parent attribute does not change the clone.");
+    });
+
+    test("Model: isNew", function() {
+        var a = new hydrogen.Model({ 'foo': 1, 'bar': 2, 'baz': 3});
+        ok(a.isNew(), "it should be new");
+        a = new hydrogen.Model({ 'foo': 1, 'bar': 2, 'baz': 3, 'id': -5 });
+        ok(!a.isNew(), "any defined ID is legal, negative or positive");
+        a = new hydrogen.Model({ 'foo': 1, 'bar': 2, 'baz': 3, 'id': 0 });
+        ok(!a.isNew(), "any defined ID is legal, including zero");
+        ok( new hydrogen.Model({          }).isNew(), "is true when there is no id");
+        ok(!new hydrogen.Model({ 'id': 2  }).isNew(), "is false for a positive integer");
+        ok(!new hydrogen.Model({ 'id': -5 }).isNew(), "is false for a negative integer");
+    });
+
+    test("Model: get", function() {
+        equal(doc.get('title'), 'The Tempest');
+        equal(doc.get('author'), 'Bill Shakespeare');
+    });
+
+    test("Model: using a non-default id attribute.", function() {
+        var MongoModel = hydrogen.Model.extend({idAttribute : '_id'});
+        var model = new MongoModel({id: 'eye-dee', _id: 25, title: 'Model'});
+        equal(model.get('id'), 'eye-dee');
+        equal(model.id, 25);
+        equal(model.isNew(), false);
+        model.unset('_id');
+        equal(model.id, null);
+        equal(model.isNew(), true);
     });
 });
